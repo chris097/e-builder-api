7115bccca7304a82ce927829462826911e75e67f
chris097 <chrisfidel.international@gmail.com>
2023-08-25 23:22:03 +0100

    login status response updated

--- controller/auth.js
+++ controller/auth.js
@@ -11,48 +11,6 @@ exports.getUsers = async (req, res) => {
     res.status(200).json({ data: auth, status: 200 })
 };
 
-// exports.registerUser = async (req, res, next) => {
-//     try {
-//         const { email, password } = req.body;
-//         const auth = new Auth({
-//             email: email,
-//             password: password
-//         });
-//         // if(!fullName) return res.status(400).json({message: 'full name is required.'})
-//         if(!email) return res.status(400).json({message: 'email is required.'})
-//         if(!password) return res.status(400).json({message: 'password is required.'})
-//         const salt = await bcrypt.genSalt(10);
-//         auth.password = await bcrypt.hash(auth.password, salt);
-//         const token = jwt.sign({ _id: auth._id, email }, JWT_TOKEN, { expiresIn: '1h' });
-//         auth.token = token;
-//         await auth.save();
-//          const transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth: {
-//                 user: `${USERNAME}`,
-//                 pass: `${PASSWORD}`
-//             }
-//         });
-//         const mailOptions = {
-//             from: 'cevBuilder',
-//             to: `${email}`,
-//             subject: 'Registered to cevBuilder',
-//             text: `http://localhost:3000/login/${auth._id}/${token}`
-//         };
-
-//         transporter.sendMail(mailOptions, function (error, info) {
-//             if (error) {
-//                 console.log(error);
-//             } else {
-//                 return res.status(201).json({ message: "Email sent successfully!" })
-//             }
-//         });
-//         res.status(201).json({ data: auth, status: 201, message: "User successfully registered!" })
-//     } catch (err) {
-//         res.status(500).json({ message: "User already exists" })
-//     }
-// };
-
 exports.registerUser = async (req, res, next) => {
     try {
         const { email, password } = req.body;
@@ -142,13 +100,13 @@ exports.loginUser = async (req, res) => {
             if (validateUser) {
                 return res.status(200).json({ data: auth, status: 201, message: "Logged in successfully!" })
             } else {
-                return res.status(400).json({ message: "email or password is incorrect" })
+                return res.status(400).json({ message: "email or password is incorrect", status: 400 })
             }
         } else {
-            return res.status(500).json({ message: "email or password is incorrect" })
+            return res.status(500).json({ message: "email or password is incorrect", status: 500 })
         }
     } catch (err) {
-        res.status(500).json({ message: err.message })
+        res.status(500).json({ message: err.message, status: 500 })
     }
 };
 
