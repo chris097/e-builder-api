3d91ea87175ca58ae43de0bcdaad40409d8150be
chris097 <chrisfidel.international@gmail.com>
2023-09-28 23:22:07 +0100

    fixed skills

--- model/technicalSkill.js
+++ model/technicalSkill.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const technicalSkill = new Schema({
-    name: {
+    name: [{
         type: String,
         required: [true, 'name is required.']
-    },
+    }],
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
