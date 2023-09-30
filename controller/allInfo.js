647ae4bb78f87073bb5047e5e494c512843fcae5
chris097 <chrisfidel.international@gmail.com>
2023-09-30 22:52:01 +0100

    all endpoint fixed

--- controller/allInfo.js
+++ controller/allInfo.js
@@ -15,15 +15,15 @@ exports.getAllInfo = async (req, res) => {
 
         if (userInfo.length === 1 && certificate.length === 0 && education.length === 0 && userExperience.length === 0 && skills.length === 0) {
              return res.status(200).json({ isCreated: true, percentage: "20", status: 200, message: "Successful" });
-        }else if (userInfo.length === 1 && certificate.length === 0 && education.length === 0 && userExperience.length === 1 && skills.length === 0) {
+        }else if (userInfo.length === 1 && certificate.length === 0 && education.length === 0 && (userExperience.length === 1 || userExperience.length === 2) && skills.length === 0) {
              return res.status(200).json({ isCreated: true, percentage: "40", status: 200, message: "Successful" });
-        }else if (userInfo.length === 1 && certificate.length === 0 && education.length === 0 && userExperience.length === 1 && skills.length === 1) {
+        }else if (userInfo.length === 1 && certificate.length === 0 && education.length === 0 && (userExperience.length === 1 || userExperience.length === 2) && skills.length === 1) {
              return res.status(200).json({ isCreated: true, percentage: "60", status: 200, message: "Successful" });
-        }else if (userInfo.length === 1 && certificate.length === 0 && education.length === 1 && userExperience.length === 1 && skills.length === 1) {
+        }else if (userInfo.length === 1 && certificate.length === 0 && education.length === 1 && (userExperience.length === 1 || userExperience.length === 2) && skills.length === 1) {
              return res.status(200).json({ isCreated: true, percentage: "80", status: 200, message: "Successful" });
-        }else if (userInfo.length === 1 && certificate.length === 1 && education.length === 1 && userExperience.length === 1 && skills.length === 1) {
+        }else if (userInfo.length === 1 && certificate.length === 1 && education.length === 1 && (userExperience.length === 1 || userExperience.length === 2) && skills.length === 1) {
              return res.status(200).json({ isCreated: true, percentage: "100%", status: 200, message: "Successful" });
-        }else res.status(404).json({ isCreated: false, percentage: "5", status: 404, message: "Not successful" });
+        }else res.status(404).json({ isCreated: false, percentage: "0", status: 404, message: "Not successful" });
        
     } catch (err) {
         res.status(500).json({message: err.message})
