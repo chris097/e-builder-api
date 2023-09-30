367d40949fd8d12da2a3f10a1c32648a9ac6ff3d
chris097 <chrisfidel.international@gmail.com>
2023-09-30 22:07:21 +0100

    all endpoint fixed

--- controller/education.js
+++ controller/education.js
@@ -3,7 +3,7 @@ const Education = require('../model/education');
 exports.getEducation = async (req, res) => {
     try {
         const userId = req.user._id;
-        const education = await Education.find(userId);
+        const education = await Education.find({userId});
     res.status(200).json({ data: education, status: 200, message: "successfully" })
     } catch (err) {
         res.status(500).json({message: err.message})
