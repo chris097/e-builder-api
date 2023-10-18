f187580148875edbbcdbfb45bc447be90b52bbf1
chris097 <chrisfidel.international@gmail.com>
2023-10-18 13:59:10 +0100

    added get percentage endpoint

--- controller/allInfo.js
+++ controller/allInfo.js
@@ -22,10 +22,38 @@ exports.getAllInfo = async (req, res) => {
         }else if (userInfo.length === 1 && certificate.length === 0 && education.length === 1 && (userExperience.length === 1 || userExperience.length === 2) && skills.length === 1) {
              return res.status(200).json({ isCreated: true, percentage: "80", status: 200, message: "Successful" });
         }else if (userInfo.length === 1 && certificate.length === 1 && education.length === 1 && (userExperience.length === 1 || userExperience.length === 2) && skills.length === 1) {
-             return res.status(200).json({ isCreated: true, percentage: "100%", status: 200, message: "Successful" });
-        }else res.status(404).json({ isCreated: false, percentage: "0", status: 404, message: "Not successful" });
+             return res.status(200).json({ isCreated: true, percentage: "100",  status: 200, message: "Successful" });
+        } else res.status(404).json({
+             isCreated: false,
+             percentage: "0",
+             status: 404,
+             message: "Not successful"
+        });
        
     } catch (err) {
         res.status(500).json({message: err.message})
     }
 };
+
+exports.getPercentage = async (req, res) => {
+     try {
+          const userId = req.user._id;
+          const userInfo = await UserInfo.find({ userId });
+          const certificate = await Certificate.find({ userId });
+          const education = await Education.find({ userId });
+          const userExperience = await UserExperience.find({ userId });
+          const skills = await TechnicalSkill.find({ userId });
+
+          return res.status(200).json({
+               isUserInfo: (userInfo.length === 1) && "20",
+               isCertificate: (certificate.length === 1) && "40",
+               isEducation: (education.length === 1) && "60",
+               isUserExp: (userExperience.length === 1 || userExperience.length === 2) && "80",
+               isSkills: (skills.length === 1) && "100",
+          });
+
+     } catch (err) {
+          res.status(500).json({ message: err.message });
+     };
+};
+
--- router/allInfo.js
+++ router/allInfo.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAllInfo } = require("../controller/allInfo");
+const { getAllInfo, getPercentage } = require("../controller/allInfo");
 const { authenticateToken } = require("../middleware/route");
 const router = express.Router();
 
 router.get('/user/information', authenticateToken, getAllInfo);
+router.get('/user/percentage', authenticateToken, getPercentage);
 
 module.exports = router;
